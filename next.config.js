27548ee85ad152ff87c10158f22b0bb110b62c94
Danish <danishpathan7477@gmail.com>
2026-06-23 01:41:57 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -36,6 +36,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8800/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
